e792d6e6b725f8492f38b9e35fb7befa40cb724b
Marcos-111201 <marcosdaniel2001@gmail.com>
2021-09-16 19:40:44 -0300

    atualização app0

--- app0/lib/main.dart
+++ app0/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter - Aplicativo Padrão',
+      title: 'Flutter Aplicativo Padrão',
       theme: ThemeData(
         // This is the theme of your application.
         //
